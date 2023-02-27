ecbfa8e1b1c34b032513b4456e0bc5119115e9c6
Christoph Zuleger <c.zuleger@cronos.de>
2023-02-27 20:28:58 +0100

    ...

--- projects/first-app/src/main.ts
+++ projects/first-app/src/main.ts
@@ -1,7 +1,44 @@
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {PreloadAllModules, provideRouter, Routes, withDebugTracing, withPreloading} from "@angular/router";
+import {provideHttpClient} from "@angular/common/http";
 
-import {AppModule} from './app/app.module';
+const routes: Routes = [
+  {
+    path: 'feature/a',
+    loadChildren: () => import('./app/feature-a/feature-a.module')
+      .then(module => module.FeatureAModule)
+  },
+  {
+    path: 'feature/b',
+    loadChildren: () => import('./app/feature-b/feature-b.module')
+      .then(module => module.FeatureBModule)
+  },
+  {
+    path: 'standalone/1',
+    loadComponent: () => import('./app/standalone-first/standalone-first.component')
+      .then(component => component.StandaloneFirstComponent)
+  },
+  {
+    path: 'standalone/2',
+    loadComponent: () => import('./app/standalone-second/standalone-second.component')
+      .then(component => component.StandaloneSecondComponent)
+  },
+  {
+    path: 'standalone/3',
+    loadComponent: () => import('./app/standalone-third/standalone-third.component')
+      .then(component => component.StandaloneThirdComponent)
+  },
+  {path: '**', redirectTo: 'feature/a'},
+];
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules),
+      withDebugTracing()
+    ),
+    provideHttpClient()
+  ]
+});
